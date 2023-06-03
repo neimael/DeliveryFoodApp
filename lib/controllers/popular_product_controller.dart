b90766314e6bdcae880f6f40e53747a0604168b4
Naima ELJID <114741266+neimael@users.noreply.github.com>
2023-06-03 20:45:45 +0100

    api functions flutter

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,21 @@
+import 'package:deliveryfood/data/repository/popular_product_repo.dart';
+import 'package:get/get_connect/http/src/response/response.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+
+  PopularProductController({required this.popularProductRepo});
+
+  List<dynamic> _popularProductList = [];
+  List<dynamic> get popularProductList => _popularProductList;
+
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+    if (response.status == 200) {
+      _popularProductList = [];
+      // _popularProductList.addAll();
+      update();
+    } else {}
+  }
+}
--- lib/helpers/dependencies.dart
+++ lib/helpers/dependencies.dart
@@ -1,3 +1,23 @@
+import 'package:deliveryfood/controllers/popular_product_controller.dart';
+import 'package:deliveryfood/data/api/api_client.dart';
+import 'package:deliveryfood/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
 Future<void> init() async {
-  
+  Get.lazyPut(
+    () => ApiClient(appBaseUrl: ""),
+  );
+
+  Get.lazyPut(
+    () => PopularProductRepo(
+      apiClient: Get.find(),
+    ),
+  );
+
+  Get.lazyPut(
+    () => PopularProductController(
+      popularProductRepo: Get.find(),
+    ),
+  );
 }
